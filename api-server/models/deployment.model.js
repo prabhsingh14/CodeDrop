c66730f8a50c4fd0023cb35c3f47938559753544
prabhsingh14 <psingh10_be21@thapar.edu>
2025-07-11 14:39:17 +0530

    Deployment model

--- api-server/models/deployment.model.js
+++ api-server/models/deployment.model.js
@@ -2,8 +2,24 @@ import mongoose from "mongoose";
 
 const deploymentSchema = new mongoose.Schema({
     owner: {
-        
-    }
-})
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    projectName: {
+        type: String,
+        required: true
+    },
+    folderPath: String, //where build lives
+    hash: String, //for comparing the current deployment with the previous one
+    isPublic: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+});
 
 export default mongoose.model("Deployment", deploymentSchema);
